2adceaa62a1d75335e243e23a5f1c121de8ce711
Michael Dera <michaeldera@live.com>
2019-08-29 11:39:25 +0200

    modified header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./header.css";
+
+
+const Header: React.FunctionComponent = () => {
+  return (
+      <header className="header-component">
+        <h1 className="header-tagline">Are you building software? We can help.</h1>
+      </header>
+  );
+}
+
+export default Header;
