449d3a26de3eebfd3eb8be0960c3d171fa5e056a
Michael Dera <michaeldera@live.com>
2019-08-30 07:40:08 +0200

    modified navigation

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import "./navigation.css";
 
 const Navigation: React.FunctionComponent = () => {
   return (
     <nav className="navigation-component">
       <div className="navigation-wrapper">
-
+        <h1>Testing</h1>
       </div>
     </nav>
   )
 }
 
-export Navigation;
+export default Navigation;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../../components/header/header';
+import Navigation from '../../components/navigation/navigation';
 
 
 const Home: React.FunctionComponent = () => {
   return (
     <React.Fragment>
       <Header/>
+      <Navigation/>
+      <div style={{height: "200vh"}}></div>
     </React.Fragment>
   );
 }
