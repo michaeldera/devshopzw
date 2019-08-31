656c5d68813f3d062bf3a91a99c13584417a7905
Michael Dera <michaeldera@live.com>
2019-08-31 21:53:19 +0200

    created cube component

---
+++ src/components/cube/cube.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Cube = () => {
+  return (
+    <div className="cube-component">
+      <div className="cube-wrapper">
+        <div className="cube">
+          <div className="cube-face-one"></div>
+          <div className="cube-face-two"></div>
+          <div className="cube-face-three"></div>
+          <div className="cube-face-four"></div>
+          <div className="cube-face-five"></div>
+          <div className="cube-face-six"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cube;
---
+++ src/components/customer-card/customer-card.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CustomerCard = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default CustomerCard;
