a283425bf1cfa3033de64a37a923ec4bcf86775f
Michael Dera <michaeldera@live.com>
2019-09-28 16:31:34 +0200

    dark home

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -22,7 +22,6 @@ const Header = () => {
         <img className="header-logo" src={devshop} alt="Devshop Logo" />
         <div className="header-content">
           <h1 className="header-tagline">Building software? We can help.</h1>
-          <p className="header-cta">See how</p>
         </div>
       </div>
     </header>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../../components/header/header';
 import CustomersSection from './sections/customers-section/customers-section';
 import Footer from '../../components/footer/footer';
-import Navigation from '../../components/navigation/navigation';
 
 
 const Home = () => {
@@ -10,7 +9,6 @@ const Home = () => {
   return (
     <React.Fragment>
       <Header/>
-      <Navigation/>
       <CustomersSection/>
       <div style={{height: "200vh"}}>
       </div>
