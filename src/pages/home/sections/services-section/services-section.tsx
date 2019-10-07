b7e051d084082cf0e92d7844e20366f010c8af91
Michael Dera <michaeldera@live.com>
2019-10-08 00:16:33 +0200

    modified blob shape

--- src/pages/home/sections/services-section/services-section.tsx
+++ src/pages/home/sections/services-section/services-section.tsx
@@ -5,8 +5,7 @@ const ServicesSection: React.FunctionComponent = () => {
   return (
     <section className="services-component">
     <div className="services-wrapper wrapper">
-      
-      <h1 className="section-heading">Join several organisations that already trust us</h1>
+         <h1 className="section-heading">Join several organisations that already trust us</h1>
       <ServicesBlob/>
     </div>
   </section>
