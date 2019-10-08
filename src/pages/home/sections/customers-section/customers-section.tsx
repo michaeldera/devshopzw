d52c0863c0a6396b9faea0cf286b7e1492e17220
Michael Dera <michaeldera@live.com>
2019-10-08 21:50:13 +0200

    modified blob

--- src/pages/home/sections/customers-section/customers-section.tsx
+++ src/pages/home/sections/customers-section/customers-section.tsx
@@ -4,39 +4,41 @@ import devshop from './logo.png';
 
 export interface ICompanyCard {
   name: string;
-  image : string;
+  image: string;
 }
 
-export const CompanyCard = ( props: ICompanyCard) => {
-  const { name, image,} = props;
+export const CompanyCard = (props: ICompanyCard) => {
+  const { name, image, } = props;
   return (
     <div className="company-card">
-      <img className="company-card-image" src={image} alt={ name + " logo"} />
+      <img className="company-card-image" src={image} alt={name + " logo"} />
     </div>
   )
 }
 
 const clients: ICompanyCard[] = [
-  { name: "Lead Construction", image: devshop},
-  { name: "MK Group", image: devshop},
-  { name: "Devshop", image: devshop}
+  { name: "Lead Construction", image: devshop },
+  { name: "MK Group", image: devshop },
+  { name: "Devshop", image: devshop }, 
+  { name: "Sidewalk", image: devshop }
 ];
 
 const CustomersSection: React.FunctionComponent = () => {
   return (
     <section className="customers-component">
-    <div className="customers-wrapper wrapper">
-      <h1 className="section-heading">You are in good company.</h1>
-      <p>Many companies already trust the Devshop to deliver their software needs</p>
-      <div className="customer-logos-grid">
-        {clients.map( client => (
-          <div key={client.name}  className="customers-grid-item">
-            <CompanyCard {...client}/>
-          </div>
-        ))}
+
+      <div className="customers-wrapper wrapper">
+        <h1 className="section-heading">You are in good company.</h1>
+        <p>Many companies already trust the Devshop to deliver their software needs</p>
+        <div className="customer-logos-grid">
+          {clients.map(client => (
+            <div key={client.name} className="customers-grid-item">
+              <CompanyCard {...client} />
+            </div>
+          ))}
+        </div>      
       </div>
-    </div>
-  </section>
+    </section>
   )
 }
 
