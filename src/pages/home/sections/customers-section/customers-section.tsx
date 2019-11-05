277023aba8dca13e343e8b90728754e3e339d269
Michael Dera <michaeldera@live.com>
2019-11-05 02:09:45 +0200

    modified customer card

--- src/pages/home/sections/customers-section/customers-section.tsx
+++ src/pages/home/sections/customers-section/customers-section.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './customer-section.css';
 import devshop from './logo.png';
+import makata from './makata.svg';
+import s3media from './s3media.png';
+import lead from './lead.jpeg'
 
 export interface ICompanyCard {
   name: string;
@@ -17,9 +20,9 @@ export const CompanyCard = (props: ICompanyCard) => {
 }
 
 const clients: ICompanyCard[] = [
-  { name: "Lead Construction", image: devshop },
-  { name: "MK Group", image: devshop },
-  { name: "Devshop", image: devshop }, 
+  { name: "Lead Construction", image: lead },
+  { name: "MK Group", image: makata },
+  { name: "S3 Media", image: s3media }, 
   { name: "Sidewalk", image: devshop }
 ];
 
@@ -29,7 +32,7 @@ const CustomersSection: React.FunctionComponent = () => {
 
       <div className="customers-wrapper wrapper">
         <h1 className="section-heading">You are in good company.</h1>
-        <p>Many companies already trust the Devshop to deliver their software needs</p>
+        <p>Many companies trust the Devshop to deliver their software and infrastructure needs</p>
         <div className="customer-logos-grid">
           {clients.map(client => (
             <div key={client.name} className="customers-grid-item">
